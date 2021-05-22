f682a29fef5c3206379c5e3f1583a0889f665902
Dilshod-01 <valiyevdilshod99@gmail.com>
2021-05-22 11:41:44 +0500

    1

---
+++ ex_1.py
@@ -0,0 +1,15 @@
+stars = {
+    'sol',
+    'alpha century',
+    'bernard',
+    'wolf 359',
+}
+print{stars [0]}
+
+peaks = {
+    'African': 'Klilimanjaro',
+    'Antarctic': 'Vinson',
+    'Australian': 'Puncak',
+    'Euroasian': 'Everest',
+}
+print(peaks[African])
